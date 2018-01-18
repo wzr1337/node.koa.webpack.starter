ad88f2c8aba6b474037ce7f94efdd776a5da308a
Dr. Patrick Bartsch <bartsch98@gmail.com>
2018-01-18 09:57:06 +0100

    feat(webpack): add production config

---
+++ webpack.config.prod.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function (x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function (mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+module.exports = {
+  entry: './src/index.ts',
+  target: 'node',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'source-map',
+  plugins: [
+    new UglifyJsPlugin({
+      test: /\.js($|\?)/i
+    })
+  ],
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  },
+  externals: nodeModules
+};
